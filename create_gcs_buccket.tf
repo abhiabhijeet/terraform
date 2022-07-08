af2238859eabd53894dacba7c87b425bbb627280
Abhijeet <88643508+abhiabhijeet@users.noreply.github.com>
2022-07-08 11:45:06 +0530

    Update create_gcs_buccket.tf

--- create_gcs_buccket.tf
+++ create_gcs_buccket.tf
@@ -10,42 +10,41 @@ terraform {
   }
 }
 
+provider "google" {
+  project = var.project_id
+  credentials = "${file("credential.json")}"
+  #project = "eternal-poetry-350701"
+  region = "us-central1"
+}
+
 variable "project_id" {
   type = string
 }
 
-provider "google" {
-  project = var.project_id
-  region  = "us-central1"
-  zone    = "us-central1-a"
+variable "bucket_name_set" {
+  description = "A set of GCS bucket names..."
+  type    = set(string)
 }
 
-# Explanation: This resource is not necessary for the creation of an GCS bucket, but is here to ensure that
-# the GCS bucket name is unique.
-#
-# Documentation: https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/id
-resource "random_id" "changeme_google_storage_bucket_simple_name" {
-  byte_length = 16
-}
 
-# GCS (Google Cloud Storage) bucket
-# Documentation: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-resource "google_storage_bucket" "changeme_google_storage_simple_bucket" {
-  name     = "changeme-${random_id.changeme_google_storage_bucket_simple_name.hex}"
-  location = "US"
-}
+resource "google_storage_bucket" "my_bucket_set" {
+  project       = "some project id should be here"
 
-module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "~> 1.6"
-  names    = ["gke-bucket"]
-  prefix = ""
-  set_admin_roles = true
-  project_id  = var.project_id
-  versioning = {
-    first = true
+  for_each      = toset(var.bucket_name_set)
+  name          = each.value     # note: each.key and each.value are the same for a set
+
+  location      = "some region should be here"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
   }
-  bucket_admins = {
-    second = "user:abhijeetgcp122@gmail.com"
- }
 }
